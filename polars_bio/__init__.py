cfcbafef0ab591ba083e36dda0186e776c2070bb
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2025-01-11 23:11:29 +0100

    fix: artifacts@v4 workaround (#64)

--- polars_bio/__init__.py
+++ polars_bio/__init__.py
@@ -16,7 +16,7 @@ logger.setLevel(logging.INFO)
 POLARS_BIO_MAX_THREADS = "datafusion.execution.target_partitions"
 
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __all__ = [
     "overlap",
     "nearest",
