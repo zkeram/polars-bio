d95ca9fd13f564d9a447b53854f83f2655f13cf5
zkeram <marek@research.zbysinski.com>
2025-02-11 18:03:05 +0000

    export pad

--- polars_bio/__init__.py
+++ polars_bio/__init__.py
@@ -3,7 +3,7 @@ from polars_bio.polars_bio import InputFormat
 from .context import ctx
 from .io import read_bam, read_fasta, read_fastq, read_table, read_vcf
 from .polars_ext import PolarsRangesOperations as LazyFrame
-from .range_op import FilterOp, nearest, overlap, merge, cluster, coverage, count_overlaps
+from .range_op import FilterOp, nearest, overlap, merge, cluster, coverage, count_overlaps, pad
 from .range_viz import visualize_intervals
 
 POLARS_BIO_MAX_THREADS = "datafusion.execution.target_partitions"
